0163bf62597e13f6140224bd5db45667bf4f79a2
imad <imadamroug89@outlook.com>
2026-02-09 00:36:19 +0100

    Changed token duration to 24 hours + another re-formating of the responses

--- routes.go
+++ routes.go
@@ -30,26 +30,29 @@ func handleLogin(c *gin.Context) {
 	result := auth.CheckPasswordHash(loginData.Password, user.Hashed_password)
 
 	if err != nil {
-		c.JSON(500, gin.H{"success": false, "reason": err.Error()})
+		c.JSON(500, gin.H{"reason": err.Error()})
 		return
 	}
 
 	if result {
 		token, err := auth.GenerateToken()
 		if err != nil {
-			c.JSON(500, gin.H{"success": false, "reason": "Token generation failed."})
+			c.JSON(500, gin.H{"reason": "Internal error"})
 			return
 		}
 
-		expiredTime := time.Now().AddDate(0, 0, 180)
+		expiredTime := time.Now().AddDate(0, 0, 1) // 24 hours
 		affectedRows, err := engine.InsertQuery("INSERT INTO sessions (token, user_id, created_at, expires_at) VALUES (?, ?, ?, ?)", *token, user.Id, time.Now().Format("2006-01-02 15:04:05"), expiredTime.Format("2006-01-02 15:04:05"))
 
 		if affectedRows >= 1 { //Checks whether the amount of rows affected is greater / equal to 1.
-			c.JSON(200, gin.H{"success": true, "token": token, "expiresAt": expiredTime.Unix()})
+			c.JSON(200, gin.H{"token": token, "expiresAt": expiredTime.Unix()})
+			return
 		}
 
+		c.JSON(500, gin.H{"reason": "Internal error"})
+
 	} else {
-		c.JSON(401, gin.H{"success": false, "reason": "Invalid credentials"})
+		c.JSON(401, gin.H{"reason": "Invalid credentials"})
 	}
 }
 
